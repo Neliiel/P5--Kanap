43583791d4d094ef14ba3e96b042a3fa88675d44
Nellie <n.miannay@orange.fr>
2022-12-23 18:42:22 +0100

    Ajout des commentaires index.js

--- front/js/index.js
+++ front/js/index.js
@@ -1,12 +1,15 @@
 /*Envoie d'une requête au site web pour récupérer les produits*/
 
+/*Recherche de l'api*/
 let articleUrl = function () {
     fetch("http://localhost:3000/api/products")
         .then(response => response.json())
         .then((data) => { console.log(data);
-
+        
+        /*Modification du HTML id=items*/
         let articlesSection = document.querySelector("#items");
 
+        /*Ajout des produits dans le HTML*/
         for (i = 0; i < data.length; i++) {
             let articlesCard = articlesSection.innerHTML += 
                                                     `<a href="./product.html?id=${data[i]._id}">
@@ -20,4 +23,5 @@ let articleUrl = function () {
     });
 };
 
+/*Appel des produits sur la page d'accueil*/
 articleUrl();
