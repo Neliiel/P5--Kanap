0132130879ff05610e31d8fad8d505f86648f2b1
Nellie <n.miannay@orange.fr>
2023-01-13 14:54:34 +0100

    Création du tableau page Panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,80 @@
-// On récupère le formulaire HTML
+//Récupération des produits du localStorage
+
+let productsInLocalStorage = JSON.parse(localStorage.getItem("products"));
+
+
+//Récupération de l'API
+
+
+//Création du tableau HTML récapitulatif des produits
+
+if (productsInLocalStorage) {
+    
+fetch("http://localhost:3000/api/products")
+.then(response => response.json())
+.then((data) => {
+  console.log(data);
+
+    for(i = 0; i < productsInLocalStorage.length; i++) {
+        let productLinea = document.getElementById("cart__items").innerHTML +=
+            `<article class="cart__item" data-id="${data[i]._id}" data-color="${data[i].colors}">
+            <div class="cart__item__img">
+              <img src="${data[i].imageUrl}" alt="Photographie d'un canapé">
+            </div>
+            <div class="cart__item__content">
+              <div class="cart__item__content__description">
+                <h2>${data[i].name}</h2>
+                <p>${productsInLocalStorage[i].color}</p>
+                <p>${data[i].price}</p>
+              </div>
+              <div class="cart__item__content__settings">
+                <div class="cart__item__content__settings__quantity">
+                  <p>Qté : </p>
+                  <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${productsInLocalStorage[i].qty}">
+                </div>
+                <div class="cart__item__content__settings__delete">
+                  <p class="deleteItem">Supprimer</p>
+                </div>
+              </div>
+            </div>
+          </article>`
+    }
+    
+})} else {
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*// On récupère le formulaire HTML
 let form = document.getElementsByClassName("cart__order__form");
 
 //************FIRST NAME************
@@ -47,4 +123,4 @@ if(testLastName) {
 } else {
     textLastName.innerHTML = 'Prénom invalide';
 }
-});
+});*/
